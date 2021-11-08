1f897b3d3519efc221a4001387fd74861418ae58
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2021-11-08 12:26:05 +0000

    Más opciones

--- gh-members.js
+++ gh-members.js
@@ -10,16 +10,28 @@ const shell = require('shelljs');
 const { Command } = require('commander');
 const program = new Command();
 
-program.version('8.3.0')
-
+if (!shell.which('git')) shell.echo("Git not installed.")
+if (!shell.which('gh')) shell.echo("gh not installed.")
 
 program
+  .version('8.3.0')
   .option('-r, --repo <repo>', 'Repository')
   .option('-o, --owner <owner>', 'Owner')
 
 program.parse(process.argv);
 
 const options = program.opts();
+
+const owner;
+
+if (!options.owner){
+  if(program.args.length < 2){
+    console.log("No arguments specified. Sending help.")
+
+    program.help();
+  }
+}
+
 
 if (options.repo) console.log(`Repository: ${options.repo}`);
 
