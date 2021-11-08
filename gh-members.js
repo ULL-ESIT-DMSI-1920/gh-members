3849106523c1c3cdcbee75fe1851a273c37baf99
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2021-11-08 12:02:53 +0000

    Añadidas opciones de prueba

--- gh-members.js
+++ gh-members.js
@@ -1,3 +1,5 @@
+#!/home/gitpod/.nvm/versions/node/v16.13.0/bin/node
+
 const ins = require("util").inspect;
 const deb = (...args) => { 
     if (debug) console.log(ins(...args, {depth: null})); 
@@ -6,3 +8,19 @@ const deb = (...args) => {
 const fs = require("fs");
 const shell = require('shelljs');
 const { Command } = require('commander');
+const program = new Command();
+
+program.version('8.3.0')
+
+
+program
+  .option('-r, --repo <repo>', 'Repository')
+  .option('-o, --owner <owner>', 'Owner')
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.repo) console.log(`Repository: ${options.repo}`);
+
+if (options.owner) console.log(`Owner: ${options.owner}`);
